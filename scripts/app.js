7a968c0a284b55df35709fb92e9bfde34dfcbeeb
Roberto Paredes <rdparedessalazar@gmail.com>
2015-10-02 15:15:29 -0500

    adds interface changes and fixes http get requests

--- scripts/app.js
+++ scripts/app.js
@@ -1,77 +1,89 @@
 "use strict";
 
 function getIcon(toggled) {
-    return (toggled === 0 ? icons['off'] : icons['on']);
+	return (toggled === 0 ? icons['off'] : icons['on']);
 }
 var icons = {
-    'on': 'done',
-    'off': 'close'
+	'on': 'done',
+	'off': 'close'
 };
 var ledsDictionary = {
-    0: 0,
-    1: 2
+	0: 0,
+	1: 2
 };
 
+angular.module('iot-app', ['ngMaterial', 'ngMdIcons', 'ngTouch'])
+	.config(function($mdThemingProvider) {
+		$mdThemingProvider.theme('default')
+			.primaryPalette('blue')
+			.accentPalette('red')
+	})
+	.controller('AppController', ['$scope', '$http', '$q', '$timeout', '$mdDialog', '$interval',
+		function($scope, $http, $q, $timeout, $mdDialog, $interval) {
+			$scope.connection = {
+				ip: '192.168.1.102',
+				port: '80'
+			};
+			$scope.ledSwitches = [0, 1].map(function(i) {
+				return {
+					'toggled': 0,
+					'icon': getIcon(0)
+				};
+			});
+			$scope.waterDistance = 0;
 
-angular.module('iot-app', ['ngMaterial', 'ngMdIcons'])
-    .config(function($mdThemingProvider) {
-        $mdThemingProvider.theme('default')
-            .primaryPalette('blue')
-            .accentPalette('red')
-    })
-    .controller('AppController', ['$scope', '$http', '$mdDialog', '$interval',
-        function($scope, $http, $mdDialog, $interval) {
-            $scope.connection = {
-                ip: '192.168.1.102',
-                port: '80'
-            };
-            $scope.switches = [0, 1].map(function(i) {
-                return {
-                    'toggled': 0,
-                    'icon': getIcon(0)
-                };
-            });
-            $scope.distance = 0;
+			$scope.setToggles = function($index) {
+				var currentItem = $scope.ledSwitches[$index];
 
-            $scope.setToggles = function($index) {
-                var requestUrl = "http://" + $scope.connection.ip + ":" + $scope.connection.port +
-                    "/digital/" + ledsDictionary[$index];
+				var requestUrl = 
+					"http://" + $scope.connection.ip + ":" + $scope.connection.port + "/digital/" 
+					+ ledsDictionary[$index] + "/"
+					+ (currentItem['toggled'] == 1 ? '0' : '1');
 
-                var responsePromise = $http.get(requestUrl);
+				$http.get(requestUrl)
+					.then(function(data) {
+						currentItem['toggled'] = data['status'];
+						currentItem['icon'] = getIcon(currentItem['toggled']);
+					}, function() {		// If error in request
+						$mdDialog.show(
+							$mdDialog.alert()
+							.parent(angular.element(document.querySelector('#mainContainer')))
+							.clickOutsideToClose(true)
+							.title('Error')
+							.content('No se pudo completar la petición.')
+							.ariaLabel('Error')
+							.ok('Hecho')
+						);
+					});
+			};
 
-                responsePromise.success(function(data, status, headers, config) {
-                    item = $scope.switches[$index];
-                    item['toggled'] == 1 ? item['toggled'] = 0 : item['toggled'] = 1;
-                    item['icon'] = getIcon($scope.switches[$index]['toggled']);
-                });
-                responsePromise.error(function() {
-                    $mdDialog.show(
-                        $mdDialog.alert()
-                        .parent(angular.element(document.querySelector('#mainContainer')))
-                        .clickOutsideToClose(true)
-                        .title('Error')
-                        .content('No se pudo completar la petición.')
-                        .ariaLabel('Error')
-                        .ok('Hecho')
-                    );
-                });
-            };
+			var deferred = $q.defer();
+			var getWaterLevel = function() {
+				var requestUrl = "http://" + $scope.connection.ip + ":" + $scope.connection.port +
+						"/distancia";
+				
+				$http.get(requestUrl, { timeout: deferred.promise })
+					.then(function() {
+						$scope.waterDistance = data['distancia'];
+					}, function(reject) {
+						if (reject.status === 0) {
+							// $http timeout
+						} else {
+							// response error status from server
+						}
+					});
+				$timeout(function() {
+					console.error("Request timeout on " + requestUrl);
+					deferred.resolve();
+				}, 2000);
+			};
 
-            var getWaterLevelTimer = function(requestUrl) {
-            	var responsePromise = $http.get(requestUrl);
-            	responsePromise.success(function(data, status, headers, config) {
-            		$scope.distance = data['distancia'];
-            	});
-            };
-
-            $scope.startGetWaterLevelTimer = function() {
-        		if ($scope.timer !== null || $scope.timer !== undefined) {
-            		$interval.cancel($scope.timer);
-        		}
-        		var requestUrl = "http://" + $scope.connection.ip + ":" + $scope.connection.port +
-            		"/distancia";
-            	getWaterLevelTimer(requestUrl);
-            	$scope.timer = $interval(function() { getWaterLevelTimer(requestUrl) }, 2000);
-            };
-        } 
-    ]);
+			var waterLevelTimer;
+			$scope.startWaterLevelTimer = function() {
+				if (angular.isDefined(waterLevelTimer) ) return;
+				
+				getWaterLevel();
+				waterLevelTimer = $interval(getWaterLevel, 2000);
+			};
+		} 
+	]);
